c25ba7c6674ba02067277c7b4a2337292520212d
notsmatch <smatchjp@gmail.com>
2020-03-14 00:16:08 +0900

    add: hh:mm:ss filter

--- frontend/app/plugins/moment-filter.js
+++ frontend/app/plugins/moment-filter.js
@@ -7,6 +7,11 @@ Vue.filter('format-date', function (value) {
   return date.format("YYYY/MM/DD HH:mm");
 })
 
+Vue.filter('format-date-seconds', function (value) {
+  const date = moment(value)
+  return date.format("YYYY/MM/DD HH:mm:ss");
+})
+
 Vue.filter('from-now', function (value) {
   const date = moment(value)
   date.locale('ja');
