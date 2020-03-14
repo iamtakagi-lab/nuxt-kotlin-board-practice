9ba2f64173514cd80fad3b3d0632fcc1cb6d24e3
notsmatch <smatchjp@gmail.com>
2020-03-15 06:04:29 +0900

    add: replies

---
+++ frontend/app/components/comment/CommentReplies.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="wrapper">
+    <h1 class="title">>>{{id}} への返信</h1>
+    <div v-if="!replies.length" class="comments">
+        返信はありません。コメントに返信してみよう。
+    </div>
+    <div v-else-if="replies.length" class="comments">
+      <Comment v-for="(comment, i) in replies" :key="i" :comment="comment" :post="post" :reply="false"/>
+    </div>
+  </section>
+</template>
+
+<script>
+import Comment from "~/components/comment/Comment";
+import $ from "jquery";
+export default {
+  components: { Comment },
+  props: ["id", "replies", "post"]
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  margin-top: 1em;
+  margin-bottom: 50px;
+  background: white;
+  max-width: 768px;
+  border-radius: 10px;
+
+  .title {
+    padding-top: .5em;
+    margin-left: 1em;
+  }
+
+  .label {
+    text-align: center;
+    background: #2581dc;
+    text-decoration: none !important;
+    margin: 0 auto;
+    transition: 0.4s;
+    color: white;
+    user-select: none; /* CSS3 */
+    -moz-user-select: none; /* Firefox */
+    -webkit-user-select: none; /* Safari、Chromeなど */
+    -ms-user-select: none; /* IE10かららしい */
+    max-width: 100%;
+    height: 50px;
+
+    .label-text {
+      line-height: 50px;
+      font-size: 20px;
+      margin: 30px;
+    }
+  }
+  .comments {
+    margin-top: .5em;
+    margin-right: 1em;
+    margin-left: 1em;
+    padding-bottom: 10px;
+  }
+}
+</style>
