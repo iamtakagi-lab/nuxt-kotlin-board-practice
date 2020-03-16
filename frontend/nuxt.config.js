2f458cc853f5ee113737cbe40c828870dc3ea348
notsmatch <smatchjp@gmail.com>
2020-03-16 16:27:05 +0900

    fix meta

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -11,13 +11,13 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'あつまれどうぶつの森　交流掲示板' },
       { hid: 'og:description', property: 'og:description', content: 'あつまれどうぶつの森　交流掲示板'  },
-      { hid: 'og:url', property: 'og:url', content: 'https://atumori.space/' },
-      { hid: 'og:title', property: 'og:title', content: 'atumori.space' },
+      { hid: 'og:url', property: 'og:url', content: 'https://atsumorichannel.net/' },
+      { hid: 'og:title', property: 'og:title', content: 'atsumorichannel.net' },
       { hid: 'og:type', property: 'og:type', content: 'website'},
       { hid: 'og:image', property: 'og:image', content: ' https://i.imgur.com/AwVDuYf.png' },
-      { hid: 'twitter:site', name: 'twitter:site', content: '@AtumoriSpace' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@AtsumoriCh' },
       { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
-      { hid: 'og:site_name', property: 'og:site_name', content: 'atumori.space' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'atsumorichannel.net' },
       { hid: 'og:locale', property: 'og:locale', content: 'ja_JP' }
     ],
     link: [
@@ -96,13 +96,13 @@ export default {
     'nuxt-fontawesome',
     '@nuxtjs/markdownit',
     ['@nuxtjs/google-analytics', {
-      id: 'UA-159980035-1'
+      id: 'UA-160712550-1'
     }],
     ['@nuxtjs/google-adsense', {
       id: 'ca-pub-793248329946064',
       pageLevelAds: true,
-      analyticsUacct: 'UA-159980035-1',
-      analyticsDomainName: 'atumori.space'
+      analyticsUacct: 'UA-160712550-1',
+      analyticsDomainName: 'atsumorichannel.net'
     }]
   ],
 
